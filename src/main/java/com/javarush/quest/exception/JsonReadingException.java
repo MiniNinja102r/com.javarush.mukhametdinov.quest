c673ccf27a3b9e08ba4736800f584868e764103a
MiniNinja102r <vadimmuxametdinov5@gmail.com>
2026-03-27 17:12:02 +0300

    Рабочая версия чтения JSON файла

---
+++ src/main/java/com/javarush/quest/exception/JsonReadingException.java
@@ -0,0 +1,7 @@
+package com.javarush.quest.exception;
+
+public final class JsonReadingException extends RuntimeException {
+    public JsonReadingException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/javarush/quest/repository/JsonGameRepository.java
+++ src/main/java/com/javarush/quest/repository/JsonGameRepository.java
@@ -3,21 +3,26 @@ package com.javarush.quest.repository;
 import com.javarush.quest.config.Config;
 import com.javarush.quest.entity.GameType;
 import com.javarush.quest.entity.Question;
+import com.javarush.quest.exception.JsonReadingException;
 import org.jetbrains.annotations.NotNull;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
 import java.io.FileReader;
+import java.net.URL;
 
 public final class JsonGameRepository implements GameRepository {
 
     @Override
     public Question read(@NotNull GameType type, int id) {
         try {
-            final String jsonPath = Config.resource.gamePath() + type.getJsonGameFile();
+            final URL jsonURL = GameRepository.class.getResource(Config.resource.gamePath() + type.getJsonGameFile());
+            if (jsonURL == null)
+                throw new JsonReadingException("Error couldn't find jsonURL: " + type.getJsonGameFile());
+
             final JSONParser jsonParser = new JSONParser();
 
-            Object parsed = jsonParser.parse(new FileReader(jsonPath));
+            Object parsed = jsonParser.parse(new FileReader(jsonURL.getPath()));
             JSONObject json = (JSONObject) parsed;
 
             String name = (String) json.get("Name");
@@ -25,7 +30,7 @@ public final class JsonGameRepository implements GameRepository {
 
             System.out.println(name + " : " + at);
         } catch (Exception e) {
-            throw new RuntimeException(e);
+            throw new JsonReadingException(e.getMessage());
         }
         return null;
     }
