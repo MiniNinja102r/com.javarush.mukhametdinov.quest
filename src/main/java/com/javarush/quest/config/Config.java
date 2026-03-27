08acabd8f7309e1d33bcae86a3b48a791946b4a1
MiniNinja102r <vadimmuxametdinov5@gmail.com>
2026-03-27 16:39:59 +0300

    Realized config version

--- src/main/java/com/javarush/quest/config/Config.java
+++ src/main/java/com/javarush/quest/config/Config.java
@@ -1,9 +1,11 @@
 package com.javarush.quest.config;
 
+import com.javarush.quest.exception.ConfigReadingException;
 import lombok.experimental.UtilityClass;
 import lombok.extern.java.Log;
 import org.yaml.snakeyaml.Yaml;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.Map;
 
@@ -15,6 +17,8 @@ public final class Config {
 
     private static Map<String, Object> data;
 
+    public static Resource resource;
+
     public static void load() {
         try (final InputStream input = Config.class.getResourceAsStream(CONFIG_PATH + SETTINGS_FILE)) {
             if (input == null) {
@@ -24,12 +28,21 @@ public final class Config {
             final Yaml yaml = new Yaml();
             data = yaml.load(input);
             loadConfig();
-        } catch (Exception e) {
-            //
+        } catch (IOException e) {
+            throw new ConfigReadingException(e.getMessage());
         }
     }
 
     private static void loadConfig() {
+        final Map<String, Object> section = (Map<String, Object>) data.get("resources");
+        if (section == null) {
+            log.severe("Resources section not found in config.");
+            return;
+        }
+        String gamePath = (String) section.get("game_path");
+        resource = new Resource(gamePath);
+    }
 
+    public record Resource(String gamePath) {
     }
 }
---
+++ src/main/java/com/javarush/quest/exception/ConfigReadingException.java
@@ -0,0 +1,7 @@
+package com.javarush.quest.exception;
+
+public final class ConfigReadingException extends RuntimeException {
+    public ConfigReadingException(String message) {
+        super(message);
+    }
+}
